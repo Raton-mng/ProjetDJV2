fb8eb9671624631ea6105cf95015034842f0afa4
Wangha <132218157+Arwangha@users.noreply.github.com>
2025-03-03 12:43:01 +0100

    Beginning of player movement

--- PokemonIIEN/Assets/Scripts/Player.cs
+++ PokemonIIEN/Assets/Scripts/Player.cs
@@ -1,4 +1,23 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
 public class Player : Trainer
 {
-    
+    [SerializeField] float moveSpeed;
+    private InputAction _moveAction;
+    private Rigidbody _rigidbody;
+    private Camera _camera;
+
+    private void Start()
+    {
+        _moveAction = InputSystem.actions.FindAction("Move");
+        _rigidbody = GetComponent<Rigidbody>();
+        _camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        Vector2 moveValue = _moveAction.ReadValue<Vector2>();
+        transform.Translate(moveValue * (Time.deltaTime * moveSpeed));
+    }
 }
