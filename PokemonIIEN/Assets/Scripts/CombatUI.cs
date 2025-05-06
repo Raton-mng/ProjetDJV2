028a5d56af7c13357fab96e2fa443595afccb574
Raton-mng <mathis.finet9@gmail.com>
2025-05-06 17:39:27 +0200

    eeeuh tmp je vais refactor le projet pour baisser les attentes

--- PokemonIIEN/Assets/Scripts/CombatUI.cs
+++ PokemonIIEN/Assets/Scripts/CombatUI.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class CombatUI : MonoBehaviour
@@ -13,9 +14,14 @@ public class CombatUI : MonoBehaviour
     public bool hasSelected;
     public Pokemon currentSelectedPokemon;
 
-    public IEnumerator SelectSinglePokemon()
+    public List<Pokemon> SelectSinglePokemon()
     {
         hasSelected = false;
         throw new NotImplementedException();
+    }
+
+    public async Task<List<Pokemon>> Test()
+    {
+        //return Task.Run(SelectSinglePokemon);
     }
 }
