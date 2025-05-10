45731037143d87ad00cd9f1e605db043a6cf713b
Wangha <132218157+Arwangha@users.noreply.github.com>
2025-05-10 15:20:52 +0200

    Update player controls

--- PokemonIIEN/Assets/Scripts/Player.cs
+++ PokemonIIEN/Assets/Scripts/Player.cs
@@ -18,6 +18,25 @@ public class Player : Trainer
     private void Update()
     {
         Vector2 moveValue = _moveAction.ReadValue<Vector2>();
-        transform.Translate(moveValue * (Time.deltaTime * moveSpeed));
+        Move(moveValue, moveSpeed);
+        //Debug.Log(moveValue);
+    }
+    
+    public void Move(Vector2 direction, float speed)
+    {
+        var rotation = transform.rotation;
+        var angles = rotation.eulerAngles;
+        angles.y = _camera.transform.rotation.eulerAngles.y;
+        rotation.eulerAngles = angles;
+        transform.rotation = rotation;
+        float horizontal = direction.x;
+        float vertical = direction.y;
+
+        Vector3 moveDirection = transform.right * horizontal + transform.forward * vertical;
+        moveDirection.x = moveDirection.x * speed;
+        moveDirection.y = _rigidbody.velocity.y;
+        moveDirection.z = moveDirection.z * speed;
+
+        _rigidbody.velocity = moveDirection;
     }
 }
