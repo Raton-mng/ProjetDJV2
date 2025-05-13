d9355eafb5ca25ca980a7f771f8852fdaa02b0f7
Wangha <132218157+Arwangha@users.noreply.github.com>
2025-05-13 19:23:46 +0200

    Enemy Movement Part 1 (script & navmeshagent)

---
+++ PokemonIIEN/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private float movementRadius;
+    private NavMeshAgent _agent;
+    private Vector3 _destination;
+    private Vector3 _defaultPosition;
+    
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _defaultPosition = transform.position;
+        StartCoroutine(RandomMovement());
+    }
+    
+    private void SetRandomDestination()
+    {
+        _destination = _defaultPosition + Random.insideUnitSphere * movementRadius;
+        _agent.SetDestination(_destination);
+    }
+
+    private IEnumerator RandomMovement()
+    {
+        while (true)
+        {
+            SetRandomDestination();
+            yield return new WaitForSeconds(Random.Range(1, 3));
+        }
+        // ReSharper disable once IteratorNeverReturns
+        // Thanks rider for these automatic suggestions lol
+    }
+}
