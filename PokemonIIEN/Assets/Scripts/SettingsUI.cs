420e3fc86f29444b415428fab8daaa8f2eb5b8dd
Wangha <132218157+Arwangha@users.noreply.github.com>
2025-05-13 20:29:11 +0200

    Push sound settings

--- PokemonIIEN/Assets/Scripts/SettingsUI.cs
+++ PokemonIIEN/Assets/Scripts/SettingsUI.cs
@@ -1,18 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider mainSlider;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider sfxSlider;
+
+    private void Start()
     {
-        
+        mainSlider.value = Mathf.Exp((audioMixer.GetFloat("MainVolume", out float mainVolume) ? mainVolume : 0.75f) / 20f);
+        musicSlider.value = Mathf.Exp((audioMixer.GetFloat("MusicVolume", out float musicVolume) ? musicVolume : 1f) / 20f);
+        sfxSlider.value = Mathf.Exp((audioMixer.GetFloat("SFXVolume", out float sfxVolume) ? sfxVolume : 1f) / 20f);
+
+        mainSlider.onValueChanged.AddListener(SetMainVolume);
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
+        sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+
+        gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetMainVolume(float value)
     {
-        
+        audioMixer.SetFloat("MainVolume", Mathf.Log(value) * 20f);
+    }
+
+    private void SetMusicVolume(float value)
+    {
+        audioMixer.SetFloat("MusicVolume", Mathf.Log(value) * 20f);
+    }
+
+    private void SetSFXVolume(float value)
+    {
+        audioMixer.SetFloat("SFXVolume", Mathf.Log(value) * 20f);
+    }
+
+    private void ToggleSettings(bool isPaused)
+    {
+        gameObject.SetActive(isPaused);
     }
 }
